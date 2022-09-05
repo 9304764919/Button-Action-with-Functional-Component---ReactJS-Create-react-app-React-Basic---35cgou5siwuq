1545c8c2a519b989856d4020ea8e96a772b14b29
Suraj Kumar <108967267+9304764919@users.noreply.github.com>
2022-09-05 12:22:29 +0530

    Update App.js
    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [p,setp]=useState("");
+  function addp(){
+    setp="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  }
   return (
     <div id="main">
       // Do not alter the main div
+    <p id="para">{p}</p>
+    <button id="click" onclick={addp}>Add</button>
+    
     </div>
   );
 }

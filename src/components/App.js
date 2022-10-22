f8c5a29d01160db917d6c5ac69c2f4a46240725e
Suraj Kumar <108967267+Suraj930@users.noreply.github.com>
2022-10-22 22:49:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [p,setp]=useState("");
-  function addp(){
-    setp="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-  }
+
+  const [show,setShow] = useState(true);
+
   return (
     <div id="main">
-      // Do not alter the main div
-    <p id="para">{p}</p>
-    <button id="click" onclick={addp}>Add</button>
-    
+      <button id="click" onClick={()=>{
+        setShow(false)
+      }}>
+        Show
+      </button>
+
+      {show? null : <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
